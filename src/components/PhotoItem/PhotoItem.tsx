7fde54e7cf7316161e9e00e13cd4c1026c80c030
Volodya <volodyaserobyan732@gmail.com>
2025-01-24 01:19:37 +0400

    added 2 comments for virtualizing will explain during interview

--- src/components/PhotoItem/PhotoItem.tsx
+++ src/components/PhotoItem/PhotoItem.tsx
@@ -23,3 +23,56 @@ const PhotoItem: React.FC<IPhotoItem> = ({ src, alt, id }) => {
 };
 
 export default React.memo(PhotoItem);
+
+// import React, { useEffect, useRef, useState } from "react";
+// import { useNavigate } from "react-router-dom";
+
+// import "./PhotoItem.scss";
+
+// interface IPhotoItem {
+//   src: string;
+//   alt: string;
+//   id: number;
+// }
+
+// const PhotoItem: React.FC<IPhotoItem> = ({ src, alt, id }) => {
+//   const navigate = useNavigate();
+//   const [isVisible, setIsVisible] = useState(false);
+//   const itemRef = useRef<HTMLDivElement>(null);
+
+//   const handleItemClick = () => {
+//     navigate(`/${id}`);
+//   };
+
+//   useEffect(() => {
+//     const observer = new IntersectionObserver(
+//       (entries) => {
+//         const [entry] = entries;
+//         setIsVisible(entry.isIntersecting);
+//       },
+//       {
+//         root: null,
+//         threshold: 0.3,
+//       }
+//     );
+
+//     const currentRef = itemRef.current;
+//     if (currentRef) {
+//       observer.observe(currentRef);
+//     }
+
+//     return () => {
+//       if (currentRef) {
+//         observer.unobserve(currentRef);
+//       }
+//     };
+//   }, []);
+
+//   return (
+//     <div onClick={handleItemClick} className="PhotoItem" ref={itemRef}>
+//       {isVisible && <img loading="lazy" src={src} alt={alt} />}
+//     </div>
+//   );
+// };
+
+// export default React.memo(PhotoItem);
--- src/pages/MasonryGridPage/MasonryGridPage.tsx
+++ src/pages/MasonryGridPage/MasonryGridPage.tsx
@@ -55,3 +55,93 @@ const MasonryGridPage: React.FC = () => {
 };
 
 export default MasonryGridPage;
+
+// import React, { useState, useEffect } from "react";
+// import PhotoItem from "../../components/PhotoItem";
+// import useInfiniteScrollPhotos from "../../hooks/useInfiniteScrollPhotos";
+// import useScrollPagination from "../../hooks/useScrollPagination";
+// import {
+//   Masonry,
+//   CellMeasurer,
+//   CellMeasurerCache,
+//   WindowScroller,
+// } from "react-virtualized";
+// import "./MasonryGridPage.scss";
+// const cache = new CellMeasurerCache({
+//   fixedWidth: true,
+//   defaultHeight: 300,
+// });
+// const MasonryGridPage: React.FC = () => {
+//   const [page, setPage] = useState<number>(1);
+//   const [hasNextPage, setHasNextPage] = useState<boolean>(true);
+//   const { photos, isNextPageAvailable, isLoading } = useInfiniteScrollPhotos(
+//     page,
+//     hasNextPage
+//   );
+//   useEffect(() => {
+//     setHasNextPage(isNextPageAvailable);
+//   }, [isNextPageAvailable]);
+//   const onScrollCallback = () => {
+//     if (isLoading || !hasNextPage) return;
+//     setPage((prevPage) => prevPage + 1);
+//   };
+//   const sentinelRef = useScrollPagination(
+//     isLoading,
+//     hasNextPage,
+//     onScrollCallback
+//   );
+//   const columnWidth = 300;
+//   const columnCount = Math.floor(window.innerWidth / columnWidth);
+//   const cellPositioner = (index: number) => {
+//     const columnIndex = index % columnCount;
+//     const rowIndex = Math.floor(index / columnCount);
+//     return {
+//       top: rowIndex * 300,
+//       left: columnIndex * columnWidth,
+//     };
+//   };
+//   return (
+//     <div className="MasonryGridPage">
+//       <div className="MasonryGridPage-cont">
+//         <WindowScroller>
+//           {({ height, isScrolling, scrollTop }) => (
+//             <Masonry
+//               autoHeight
+//               cellCount={photos.length}
+//               cellMeasurerCache={cache}
+//               cellPositioner={cellPositioner}
+//               cellRenderer={({ index, key, parent, style }) => {
+//                 const photo = photos[index];
+//                 return (
+//                   <CellMeasurer
+//                     key={key}
+//                     cache={cache}
+//                     columnIndex={0}
+//                     rowIndex={index}
+//                     parent={parent}
+//                   >
+//                     <div style={style}>
+//                       <PhotoItem
+//                         key={photo.id}
+//                         src={photo.src.original}
+//                         alt={photo.alt}
+//                         id={photo.id}
+//                       />
+//                     </div>
+//                   </CellMeasurer>
+//                 );
+//               }}
+//               height={height}
+//               isScrolling={isScrolling}
+//               scrollTop={scrollTop}
+//               width={window.innerWidth}
+//             />
+//           )}
+//         </WindowScroller>
+//       </div>
+//       <div ref={sentinelRef} className="sentinel"></div>
+//       {isLoading && <div className="loading-spinner">Loading...</div>}
+//     </div>
+//   );
+// };
+// export default MasonryGridPage;
